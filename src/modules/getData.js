e1f75968a1c1ba67c071c0ed2d2cf56111944e3e
Fred-devv <fedorkuhta@yandex.ru>
2025-02-07 16:44:30 +0300

    Получение данных с сервера Firebase-JSON

---
+++ src/modules/getData.js
@@ -0,0 +1,8 @@
+const getData = () => {
+    return fetch('https://test-e4411-default-rtdb.firebaseio.com/goods.json')
+        .then((response) => {
+            return response.json()
+        })
+}
+
+export default getData
